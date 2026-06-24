43f2e5ff81c037bf6994dc2736f1f10c9e318232
Ana Júlia <celchior@gmail.com>
2026-06-24 19:22:19 -0300

    Converte projeto para sistema dinamico com models

--- meuapp/models.py
+++ meuapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Personagem(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.CharField(max_length=50)
+    origem = models.CharField(max_length=100)
+    imagem = models.ImageField(upload_to='personagens/')
+
+    def __str__(self):
+        return self.nome
--- meuapp/views.py
+++ meuapp/views.py
@@ -1,92 +1,12 @@
 from django.shortcuts import render
-
+from .models import Personagem
 
 def index(request):
     return render(request, "meuapp/index.html")
 
 
 def elenco(request):
-
-    personagens = [
-
-        {
-            "nome": "Mônica",
-            "idade": "10 anos",
-            "origem": "Bairro do Limoeiro",
-            "imagem": "imagens/monica.jpg"
-        },
-
-        {
-            "nome": "Cebolinha",
-            "idade": "10 anos",
-            "origem": "Bairro do Limoeiro",
-            "imagem": "imagens/cebolinha.jpg"
-        },
-
-        {
-            "nome": "Cascão",
-            "idade": "10 anos",
-            "origem": "Bairro do Limoeiro",
-            "imagem": "imagens/cascao.jpg"
-        },
-
-        {
-            "nome": "Magali",
-            "idade": "10 anos",
-            "origem": "Bairro do Limoeiro",
-            "imagem": "imagens/magali.jpg"
-        },
-
-        {
-            "nome": "Chico Bento",
-            "idade": "10 anos",
-            "origem": "Vila Abobrinha",
-            "imagem": "imagens/chicobento.jpg"
-        },
-
-        {
-            "nome": "Marina",
-            "idade": "10 anos",
-            "origem": "Bairro do Limoeiro",
-            "imagem": "imagens/marina.jpg"
-        },
-
-        {
-            "nome": "Franjinha",
-            "idade": "10 anos",
-            "origem": "Bairro do Limoeiro",
-            "imagem": "imagens/franjinha.jpg"
-        },
-
-        {
-            "nome": "Dorinha",
-            "idade": "10 anos",
-            "origem": "Bairro do Limoeiro",
-            "imagem": "imagens/dorinha.webp"
-        },
-
-        {
-            "nome": "Xaveco",
-            "idade": "10 anos",
-            "origem": "Bairro do Limoeiro",
-            "imagem": "imagens/xaveco.png"
-        },
-
-        {
-            "nome": "Do Contra",
-            "idade": "10 anos",
-            "origem": "Bairro do Limoeiro",
-            "imagem": "imagens/docontra.jpg"
-        },
-
-        {
-            "nome": "Bidu",
-            "idade": "Desconhecida",
-            "origem": "Bairro do Limoeiro",
-            "imagem": "imagens/bidu.jpg"
-        }
-
-    ]
+    personagens = Personagem.objects.all()
 
     context = {
         "personagens": personagens
